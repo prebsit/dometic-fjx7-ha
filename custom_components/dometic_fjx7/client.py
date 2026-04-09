b3cf7280b02a335fd6f232929e7872c16166f781
prebsit <github@prebs.it>
2026-04-09 19:50:58 +0200

    Fix: back to Write Request (response=True) with 50ms pacing - FJX7 needs ATT ack

--- custom_components/dometic_fjx7/client.py
+++ custom_components/dometic_fjx7/client.py
@@ -146,7 +146,7 @@ class FJX7BLEClient:
                 cmd = encode_subscribe(param)
                 _LOGGER.debug("FJX7: writing subscribe %d/%d param=0x%02x", i+1, len(SUBSCRIBE_PARAMS), param)
                 try:
-                    await client.write_gatt_char(WRITE_UUID, cmd, response=False)
+                    await client.write_gatt_char(WRITE_UUID, cmd, response=True)
                     success_count += 1
                 except Exception as write_err:
                     _LOGGER.debug("FJX7: subscribe write failed at %d/%d: %s", i+1, len(SUBSCRIBE_PARAMS), write_err)
@@ -210,7 +210,7 @@ class FJX7BLEClient:
             self._connected = True
 
             await client.start_notify(NOTIFY_UUID, on_notify)
-            await client.write_gatt_char(WRITE_UUID, data, response=False)
+            await client.write_gatt_char(WRITE_UUID, data, response=True)
             _LOGGER.debug("FJX7: command sent, waiting for echo")
 
             try:
